b721f7d4040a98be8453ee8889476d1cd8be326b
Ole Morten Didriksen <code@oledid.com>
2016-07-27 13:17:24 +0200

    Fix error in teamcity.ps1

--- teamcity.ps1
+++ teamcity.ps1
@@ -6,7 +6,7 @@ node .\src\cli.js
 # run xo
 write-host "##teamcity[testSuiteStarted name='xo']"
 .\node_modules\.bin\xo --reporter tap | .\node_modules\.bin\tap-teamcity.cmd
-write-host "##teamcity[testSuiteEnded name='xo']"
+write-host "##teamcity[testSuiteFinished name='xo']"
 
 # run tests
 .\node_modules\.bin\cucumber-js.cmd --format json | .\node_modules\.bin\cucumber-json-to-teamcity.cmd
